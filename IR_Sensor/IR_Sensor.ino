8c4c59baa4c369664be4b69ddf26026bd15d63c7
Khaled <khaled@robotical.io>
2017-08-29 15:15:38 +0100

    Added comments

--- IR_Sensor/IR_Sensor.ino
+++ IR_Sensor/IR_Sensor.ino
@@ -14,54 +14,41 @@ void write_register(int device_addr, int register_num, int low_byte, int high_by
 
 
 void setup() {
+  
   Serial.begin(115200);
   Wire.begin();
 
+  //Write to PS_CONF1 and PS_CONF2 to set Proximity Sensor (PS) settings.
   write_register(SENSOR_ADDR, 0x3, B00011010, B00001000);
-//  Wire.beginTransmission(SENSOR_ADDR);
-//  Wire.write(0x3);
-//  Wire.write(B00011010);  //PS_CONF1
-//  Wire.write(B00001000);  //PS_CONF2
-//  Wire.endTransmission(false);
-
+ 
+  //Write to ALS_CONF to turn Ambient Light Sensor (ALS) off.
   write_register(SENSOR_ADDR, 0x0, B01000001, B00000000);
-//  Wire.beginTransmission(SENSOR_ADDR);
-//  Wire.write(0x0);
-//  Wire.write(B01000001);  //ALS_CONF  
-//  Wire.write(B00000000);  //RESERVED
-//  Wire.endTransmission(false);
-
+ 
+  //Write to PS_THDL_L and PS_THDL_H to set Proxmitt Sensor (PS) Threshhold.
   write_register(SENSOR_ADDR, 0x6, B00010000, B00000000);  
-//  Wire.beginTransmission(SENSOR_ADDR);
-//  Wire.write(0x6);
-//  Wire.write(B00010000);  //PS_CONF1
-//  Wire.write(B00000000);  //PS_CONF2
-//  Wire.endTransmission(false);
-
+ 
+  //Write to PS_CONF3 and PS_MS to set Proximity Sensor (PS) settings.
   write_register(SENSOR_ADDR, 0x4, B01110000, B00000111);
-//  Wire.beginTransmission(SENSOR_ADDR);
-//  Wire.write(0x4);
-//  Wire.write(B01110000);  //PS_CONF3
-//  Wire.write(B00000111);  //PS_MS
-//  Wire.endTransmission(false);
+
 }
 
 void loop() {
 
-  int reg = 0x8;
-  
+  int reg = 0x8; //Proximity Sensor (PS) output
+
+  //Write to regsiter
   Wire.beginTransmission(SENSOR_ADDR);
   Wire.write(reg);
   Wire.endTransmission(false);
 
   //Read data
-  byte data[2] = {0, 0};
+  byte data[2] = {0, 0};  //The sensor provides the output over 2 bytes.
   Wire.requestFrom(SENSOR_ADDR, 2);
   data[0] = Wire.read();
   data[1] = Wire.read();
-  int value = int(data[1])*256 + int(data[0]);
-  Serial.print(value);
-  Serial.println(",");
+  int value = int(data[1])*256 + int(data[0]); //Combines bytes
+  Serial.println(value);
+
   delay(100);
 
 }
